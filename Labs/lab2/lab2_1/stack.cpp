0dac17f77b8a9abc38e1890693f810dba431df49
Spencer Tapp <spentapp@outlook.com>
2026-02-04 13:55:22 -0500

    Did all of lab 2

--- Labs/lab2/lab2_1/stack.cpp
+++ Labs/lab2/lab2_1/stack.cpp
@@ -1,3 +1,4 @@
+#include "Arduino.h"
 /******************************************************************************
 FILENAME:   stack.cpp 
 COURSE:     MREN 178
@@ -25,7 +26,12 @@ void CreateStackArray(Stack stack_array[], int num_stacks){
 int Push(pStack stack, long timestamp, char data) {
   
   /*-------------------------insert your code here--------------------------*/
-
+  if (!IsStackFull(stack)) {
+    stack->top ++;
+    stack->stack_of_items[stack->top].data = data;
+    stack->stack_of_items[stack->top].timestamp = timestamp;
+    return EXIT_SUCCESS;
+  }
   // this return value will need to be changed once your code is written
   return EXIT_ERR;
 }
@@ -33,31 +39,49 @@ int Push(pStack stack, long timestamp, char data) {
 int Pop(pStack stack, long &timestamp, char &data) {
   
   /*-------------------------insert your code here--------------------------*/
-
+  if (!IsStackEmpty(stack)) {
+    timestamp = stack->stack_of_items[stack->top].timestamp;
+    data = stack->stack_of_items[stack->top].data;
+    stack->top --;
+    return EXIT_SUCCESS;
+  }
   // this return value will need to be changed once your code is written
   return EXIT_ERR;
 }
 
 bool IsStackFull (pStack stack) {
   /*-------------------------insert your code here--------------------------*/
-
   // this return value will need to be changed once your code is written
-  return false;
+  return stack->top == MAX_STACK_LEN-1;
+  
 }
 
 bool IsStackEmpty (pStack stack) {
   /*-------------------------insert your code here--------------------------*/
-
+  return stack->top==-1;
 }
 
 void PopAll(pStack stack) {
   /*-------------------------insert your code here--------------------------*/
-
+  stack->top = -1;
 }
 
 long GetTime(DS3231 rtc_clock) {
   /*-------------------------insert your code here--------------------------*/
+  long seconds;
+  seconds += rtc_clock.getSecond();
+  seconds += rtc_clock.getMinute()*60;
+  bool h12Flag;
+  bool pmFlag;
+  byte hours = rtc_clock.getHour(h12Flag, pmFlag);
+  if (h12Flag) {
+    if (pmFlag) {
+      seconds += 12*3600 + 3600*hours;
+    }
+    else seconds += 3600*hours;
+  }
+  else seconds += 3600*hours;
   
   // this return value will need to be changed once your code is written
-  return 0;
+  return seconds;
 }
--- Labs/lab2/lab2_2/queue.cpp
+++ Labs/lab2/lab2_2/queue.cpp
@@ -1,10 +1,11 @@
+#include "HardwareSerial.h"
 /******************************************************************************
 FILENAME:   queue.cpp 
 COURSE:     MREN 178
 LAB:        2 
 
-STUDENT:   [YOUR NAME HERE]         STUDENT ID: [YOUR STUDENT ID]  
-PARTNER:   [PARTNER's NAME HERE]    
+STUDENT:   Spencer Tapp        STUDENT ID: 20512353  
+PARTNER:   Owen Reid                       20516094
 
 OTHER CONTRIBUTORS: [AS PART OF ACADEMIC INTEGRITY POLICY, ANY CONTRIBUTIONS
 OBTAINED FROM ADDITIONAL SOURCES MUST BE LISTED. THESE INCLUDE WEBSITES, OTHER
@@ -54,35 +55,63 @@ pStampedItem CreateItem (long timestamp) {
 
 bool IsQEmpty (pQueue queue) {
   /*-------------------------insert your code here--------------------------*/
-
+  if (queue->count == 0) return true;
   // this return value may need to be changed once your code is written
-  return true;
+  return false;
 }
 
 int Enqueue (pQueue queue, pStampedItem item) {
   /*-------------------------insert your code here--------------------------*/
-
+  if(queue->back != NULL) queue->back->next = item;
+  else queue->front = item;
+  queue->back = item;
+  queue->count++;
+  Serial.println(queue->count);
   // this return value may need to be changed once your code is written
-  return EXIT_ERR;
+  return EXIT_OK;
 }
 
 int Dequeue (pQueue queue, long &timestamp) {
   /*-------------------------insert your code here--------------------------*/
-
+  if (IsQEmpty(queue)) {
+    Serial.println(queue->count);
+    return EXIT_FAILURE;  
+  }
+  timestamp = queue->front->timestamp;
+  pStampedItem delete_this = queue->front;
+  queue->front = queue->front->next;
+  free(delete_this);
+  queue->count --;
   // this return value may need to be changed once your code is written
-  return EXIT_ERR;
+  return EXIT_SUCCESS;
 }
 
 int DequeueAll (pQueue queue) {
   /*-------------------------insert your code here--------------------------*/
-
+  long timestamp;
+  while (queue->count > 0) {
+    Dequeue(queue, timestamp);
+  }
   // this return value may need to be changed once your code is written
-  return EXIT_ERR; 
+  return EXIT_SUCCESS; 
 }
 
 long GetTime(DS3231 rtc_clock) {
   /*-------------------------insert your code here--------------------------*/
-
-  // this return value may need to be changed once your code is written
-  return 0;
+  long seconds;
+  seconds += rtc_clock.getSecond();
+  seconds += rtc_clock.getMinute()*60;
+  bool h12Flag;
+  bool pmFlag;
+  byte hours = rtc_clock.getHour(h12Flag, pmFlag);
+  if (h12Flag) {
+    if (pmFlag) {
+      seconds += 12*3600 + 3600*hours;
+    }
+    else seconds += 3600*hours;
+  }
+  else seconds += 3600*hours;
+  
+  // this return value will need to be changed once your code is written
+  return seconds;
 }
